33c2050de155a3d859bd8297b2661c7482eb7585
Ananta Durga Prasad <ananta.durgaprasad@wizcart.com>
2025-02-03 20:34:48 +0530

    correcting arn

--- infra/eks.tf
+++ infra/eks.tf
@@ -90,7 +90,7 @@ locals {
   mandatory_helm_chart_values = {
     "clusterName"                                               = module.eks.eks_cluster_name
     "serviceAccount.name"                                       = var.service_account
-    "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.irsa.role_arn
+    "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" = module.controller.irsa_role_arn
   }
 
 }
