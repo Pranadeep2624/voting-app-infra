d3ce68c3d20f4b52beff4d0b96cccaa18782cce3
Ananta Durga Prasad <ananta.durgaprasad@wizcart.com>
2025-02-06 15:26:17 +0530

    deleting cluster

--- infra/eks.tf
+++ infra/eks.tf
@@ -3,20 +3,20 @@ data "aws_caller_identity" "current" {
 }
 
 
-module "eks" {
-  source                      = "git::https://github.com/Pranadeep2624/terraform-aws-modules.git//EKS"
-  environment                 = var.environment
-  app_name                    = var.app_name
-  eks_version                 = var.eks_version
-  subnet_ids                  = module.vpc.private_subnets_id
-  eks_instance_type           = var.eks_instance_type
-  eks_desired_size            = var.eks_desired_size
-  eks_min_size                = var.eks_min_size
-  eks_max_size                = var.eks_max_size
-  eks_endpoint_private_access = var.eks_endpoint_private_access
-  eks_endpoint_public_access  = var.eks_endpoint_public_access
-  depends_on                  = [module.vpc]
-}
+# module "eks" {
+#   source                      = "git::https://github.com/Pranadeep2624/terraform-aws-modules.git//EKS"
+#   environment                 = var.environment
+#   app_name                    = var.app_name
+#   eks_version                 = var.eks_version
+#   subnet_ids                  = module.vpc.private_subnets_id
+#   eks_instance_type           = var.eks_instance_type
+#   eks_desired_size            = var.eks_desired_size
+#   eks_min_size                = var.eks_min_size
+#   eks_max_size                = var.eks_max_size
+#   eks_endpoint_private_access = var.eks_endpoint_private_access
+#   eks_endpoint_public_access  = var.eks_endpoint_public_access
+#   depends_on                  = [module.vpc]
+# }
 
 resource "aws_ecr_repository" "voting_app_ecrs" {
   for_each             = toset(var.applications)
